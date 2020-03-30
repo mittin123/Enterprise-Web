9a9a490045f66f7d7e0df919d24146e0c48b813f
mittin123 <kyoshirox1104@gmail.com>
2020-03-31 02:25:33 +0700

    config

--- #config.php
+++ #config.php
@@ -1,3 +1,21 @@
 <?php
 //Config file for database and other else
+class Database{
+    private $host = 'localhost';
+    private $db_name = 'waiterless';
+    private $user = 'root';
+    private $pw = '';
+    public $connect;
+
+    public function connect(){
+        $this->connect = null;
+        try {
+            $this->connect = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name,$user,$pw);
+            $this->connect->exec("set names UTF8");
+        } catch (PDOException $ex) {
+            echo "Connect to database error. Message: ".$ex->getMessage();
+        }
+        return $this->connect; 
+    }
+}
 ?>
