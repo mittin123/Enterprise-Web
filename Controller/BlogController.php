7dd133cd8b3d4c139f8fefa630e120c2d5131f8c
huyhuy1412 <huynxgch17328@fpt.edu.vn>
2020-04-15 22:53:52 +0700

    Update BlogController.php

--- Controller/BlogController.php
+++ Controller/BlogController.php
@@ -2,14 +2,33 @@
 include("#config.php");
 include("Model/Blog.php");
 include("Model/Layout.php");
+
+
 class BlogController extends LayoutController{
     public function index(){
         $this->loadView('layout');
     }
+    public function viewBlog($user){
 
-}
-if(!isset($_SESSION)){
-    session_start();
+    }
+    
+    public function createBlog($user, $title, $abstraction, $content, $url, $create_time){
+        $addB = new BlogController();
+        $result = $addB->createBlog($user, $title, $abstraction, $content, $url, $create_time)
+        if($result){
+            echo 'Add Successful!';
+        } else{
+            echo 'Add Fail!';
+        }
+    }
 
+    public function updateBlog($user, $id, $title, $abstraction, $content, $url, $create_time){
+
+    }
+
+    public function deleteBlog($user, $id){
+
+    }
 }
+
 ?>
