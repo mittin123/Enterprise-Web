1569450d95b77bb1445580c6a20cbe9080da775f
huyhuy1412 <huynxgch17328@fpt.edu.vn>
2020-04-15 18:10:25 +0700

    Update BlogController.php

--- Controller/BlogController.php
+++ Controller/BlogController.php
@@ -1,7 +1,15 @@
 <?php
+include("#config.php");
+include("Model/Blog.php");
+include("Model/Layout.php");
 class BlogController extends LayoutController{
     public function index(){
         $this->loadView('layout');
     }
+
+}
+if(!isset($_SESSION)){
+    session_start();
+
 }
 ?>
