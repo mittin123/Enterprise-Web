79f7efb8b37d60078b854b8bb21f0cd2138f8435
huyhuy1412 <huynxgch17328@fpt.edu.vn>
2020-04-16 04:16:58 +0700

    Update BlogController.php

--- Controller/BlogController.php
+++ Controller/BlogController.php
@@ -2,18 +2,26 @@
 include("#config.php");
 include("Model/Blog.php");
 include("Model/Layout.php");
-
+include("login.php")
 
 class BlogController extends LayoutController{
     public function index(){
         $this->loadView('layout');
     }
-    public function viewBlog($user){
-
+    public function viewAllBlog(){
+        $vieB = new Blog();
+        $list = $vieB->view_all_blog($_SESSION['id']);
+        return $list;
     }
     
+    public function viewBlogDetail($id){
+        $vieB = new Blog();
+        $detail = $vieB->view_blog_detail($_SESSION['id'], $id);
+        return $detail;
+    }
+
     public function createBlog($user, $title, $abstraction, $content, $url, $create_time){
-        $addB = new BlogController();
+        $addB = new Blog();
         $result = $addB->create_blog($user, $title, $abstraction, $content, $url, $create_time)
         if($result){
             echo 'Add Successful!';
@@ -23,7 +31,7 @@ class BlogController extends LayoutController{
     }
 
     public function updateBlog($user, $id, $title, $abstraction, $content, $url, $create_time){
-        $updB = new BlogController();
+        $updB = new Blog();
         $result = $updB->update_blog($user, $id, $title, $abstraction, $content, $url, $create_time)
         if($result){
             echo 'Update Successful!';
